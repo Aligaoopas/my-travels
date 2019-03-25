1db55befc7b22da5c90526fa330629c9abdebcfd
Remi <aligastudioopas@gmail.com>
2019-03-25 10:59:52 +0100

    first commit my-travel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Quote from "./Travel";
 
 class App extends Component {
   render() {
@@ -8,18 +11,20 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Destination</h1>
         </header>
+        <Quote
+          destination="Toulouse"
+          country="France"
+          image="https://www.edreams.com/blog/wp-content/uploads/sites/3/2018/02/Toulouse-Saint-Pierre-Bridge.jpg"
+          distance="883"
+        />
+        <Quote
+          destination="Moscou"
+          country="Russie"
+          image="https://www.voyageursdumonde.fr/vdm/imgProduits/RUS/408728.ori.jpg"
+          distance="2 594"
+        />
       </div>
     );
   }
---
+++ src/Travel.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Travel = ({ destination, country, image, distance }) => (
+    <figure>
+      <img src={image} alt={country} />
+      <figcaption>
+        <h1>{destination}</h1>
+        <h2>{country}</h2>
+        <p>{distance} km te sépare de cette destination si tu es de Lille</p>
+      </figcaption>
+    </figure>
+  );
+
+export default Travel;
